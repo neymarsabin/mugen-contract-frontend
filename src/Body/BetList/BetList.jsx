51db4240b889d970acd37abd6dbfdc4e3a7114ee
Sabin Nepal <reddevil.sabin@gmail.com>
2021-07-08 21:00:36 +0545

    start bet list left sidebar design

--- src/Body/BetList/BetList.jsx
+++ src/Body/BetList/BetList.jsx
@@ -1,11 +1,43 @@
 import "./styles.css";
+import { Container, Row, Col } from 'react-bootstrap';
+
+const BetListRow = ({ blueLabel, redLabel }) => {
+  return(
+    <Row className="justify-content-space-between">
+      <Col>
+        <span className="bet-option-blue">{blueLabel}</span>
+      </Col>
+      <Col>
+        <span className="bet-option-red">{blueLabel}</span>
+      </Col>
+    </Row>
+  );
+};
+
+const sampleData = [
+  {
+    blueLabel: 'Option 1',
+    redLabel: 'Option 2'
+  },
+  {
+    blueLabel: 'Option 1',
+    redLabel: 'Option 2'
+  }
+];
 
 const BetList = () => {
-	return (
-		<div className="betlist-box">
-			{/* <h1>Bet list </h1> */}
-		</div>
-	);
+  return (
+    <Container>
+      { sampleData.map((sData) => {
+        return(
+          <BetListRow
+            blueLabel={sData.blueLabel}
+            redLabel={sData.redLabel}
+          />
+        );
+      })}
+    </Container>
+  );
 };
 
 export default BetList;
